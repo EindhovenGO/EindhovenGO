66512b0b178d5f8e4e615892a867d914032402a2
Joey Lensen <jojolensen@outlook.com>
2020-03-19 19:42:57 +0100

    Make it so the maps layout can actually be altered. Add styles to the map

--- app/src/main/java/com/example/eindhovengo/MapsActivity.java
+++ app/src/main/java/com/example/eindhovengo/MapsActivity.java
@@ -1,10 +1,7 @@
 package com.example.eindhovengo;
 
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.FragmentActivity;
-
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
@@ -12,17 +9,18 @@ import android.location.Criteria;
 import android.location.LocationManager;
 import android.os.Bundle;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.MapStyleOptions;
 
-import java.security.Provider;
-
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends Activity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
@@ -31,7 +29,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
@@ -49,33 +47,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        // hide most of the point of interest indicators
+        mMap.setMapStyle(new MapStyleOptions(getResources().getString(R.string.style_json)));
 
         // Check if we have permission to use user location
         // otherwise ask for it
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
             if (mMap != null) {
-                // enable button to focus camera on user location
-                mMap.setMyLocationEnabled(true);
-
-                // we ask for the best provider using a default criteria
-                String provider = locationManager.getBestProvider(new Criteria(), true);
-
-                // when we can ge the location we focus on it, otherwise we don't do
-                // anything
-                if (provider != null) {
-                    double lat = locationManager.getLastKnownLocation(provider).getLatitude();
-                    double lng = locationManager.getLastKnownLocation(provider).getLongitude();
-                    LatLng location = new LatLng(lat, lng);
-
-
-                    // focus on user location when map screen opened and zoom
-                    mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
-                    mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
-                }
+                focusUserLocation();
             }
         } else {
+            // TODO: fix the case where user denies app permissions again
             ActivityCompat.requestPermissions(this,
                     new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         }
@@ -91,5 +75,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         cOptions.strokeWidth(0);
         map.addCircle(cOptions);
     }
+
+    public void focusUserLocation() throws SecurityException{
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        // enable button to focus camera on user location
+        mMap.setMyLocationEnabled(true);
+
+        // we ask for the best provider using a default criteria
+        String provider = locationManager.getBestProvider(new Criteria(), true);
+
+        // when we can ge the location we focus on it, otherwise we don't do
+        // anything
+        if (provider != null) {
+            double lat = locationManager.getLastKnownLocation(provider).getLatitude();
+            double lng = locationManager.getLastKnownLocation(provider).getLongitude();
+            LatLng location = new LatLng(lat, lng);
+
+
+            // focus on user location when map screen opened and zoom
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+            // just show a circle right now, to show it works
+            addCircle(mMap, new LatLng(lat, lng), 3000);
+        }
+    }
+
 
 }
