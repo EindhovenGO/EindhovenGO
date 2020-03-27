1fe46acc846bc1839b5c4e1f75b6f7040c73c862
LeSerj <51410126+LeSerj@users.noreply.github.com>
2020-03-27 17:39:34 +0100

    Added some fixes regarding string operations

--- app/src/main/java/com/example/endgo/MainActivity.java
+++ app/src/main/java/com/example/endgo/MainActivity.java
@@ -89,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
         fDB.child("Users").child(userKey).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                //name = dataSnapshot.child("name").getValue().toString();
+                name = dataSnapshot.child("name").getValue().toString();
                 username.setText(name);
             }
 
@@ -132,6 +132,8 @@ public class MainActivity extends AppCompatActivity {
                     pwdReset.setError("New password is empty");
                 } else if (newPwd.length() < 6) {
                     pwdReset.setError("Password must be at least 6 characters long");
+                } else if (newPwd.length() > 24) {
+                    pwdReset.setError("Password must be at most 24 characters long");
                 } else {
                     AuthCredential credential = EmailAuthProvider.getCredential(fUser.getEmail(), oldPwd);
                     fUser.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
